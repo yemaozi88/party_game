4e34beaf9a4d6f262571e1ed4fdfd46af8c6cc44
yemaozi88 <428968@gmail.com>
2019-03-25 23:34:11 +0100

    a simple speech recognition function is added.

--- party_game/Program.cs
+++ party_game/Program.cs
@@ -3,13 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
 
 namespace party_game
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task RecognizeSpeechAsync()
         {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            var config = SpeechConfig.FromSubscription("87b20fb26e8242de913d680b5213e9b4", "northeurope");
+
+            // Creates a speech recognizer.
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something...");
+
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result. 
+                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                // shot recognition like command or query. 
+                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync();
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"We recognized: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            RecognizeSpeechAsync().Wait();
+            Console.WriteLine("Please press a key to continue.");
+            Console.ReadLine();
         }
     }
 }
