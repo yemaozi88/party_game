9c83efedce9fdbe3fed30fda5c7dcbc9e4856735
Arnaud van Galen <arnaudvangalen@hotmail.com>
2019-03-29 10:54:01 +0100

    Fully functional, some ToDo's

---
+++ party_game_ui/Game.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace party_game_ui
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.Answer = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.UseSpeech = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(259, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(506, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(506, 194);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(506, 376);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "";
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(261, 376);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "";
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(12, 376);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "";
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(14, 194);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "";
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImage = global::party_game_ui.Properties.Resources.default_tile;
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox9.Location = new System.Drawing.Point(261, 194);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(241, 176);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "";
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Answer.Location = new System.Drawing.Point(14, 560);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(488, 44);
+            this.Answer.TabIndex = 9;
+            this.Answer.TextChanged += new System.EventHandler(this.Answer_TextChanged);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(508, 560);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(77, 44);
+            this.Reset.TabIndex = 10;
+            this.Reset.Text = "&Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // UseSpeech
+            // 
+            this.UseSpeech.Location = new System.Drawing.Point(647, 560);
+            this.UseSpeech.Name = "UseSpeech";
+            this.UseSpeech.Size = new System.Drawing.Size(100, 44);
+            this.UseSpeech.TabIndex = 11;
+            this.UseSpeech.Text = "Use &Speech";
+            this.UseSpeech.UseVisualStyleBackColor = true;
+            this.UseSpeech.Click += new System.EventHandler(this.UseSpeech_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::party_game_ui.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(754, 616);
+            this.Controls.Add(this.UseSpeech);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Game";
+            this.Tag = "0";
+            this.Text = "Zoek de schat";
+            this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.TextBox Answer;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button UseSpeech;
+    }
+}
+
---
+++ party_game_ui/Game.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace party_game_ui
+{
+    public partial class Game : Form
+    {
+        //ToDo: Implement visibility so the background becomes less transparent with every success
+        //ToDo: Implement resizable UI so the application can be maximized
+        //ToDo: Use proper Resources for Strings like " Zoek de schat", "Reset" and "Use Speech"
+        public int visibility = 255;
+        public Game()
+        {
+            InitializeComponent();
+        }
+        private void Game_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Tag = Properties.Settings.Default.Tile1Word;
+            pictureBox2.Tag = Properties.Settings.Default.Tile2Word;
+            pictureBox3.Tag = Properties.Settings.Default.Tile3Word;
+            pictureBox4.Tag = Properties.Settings.Default.Tile4Word;
+            pictureBox5.Tag = Properties.Settings.Default.Tile5Word;
+            pictureBox6.Tag = Properties.Settings.Default.Tile6Word;
+            pictureBox7.Tag = Properties.Settings.Default.Tile7Word;
+            pictureBox8.Tag = Properties.Settings.Default.Tile8Word;
+            pictureBox9.Tag = Properties.Settings.Default.Tile9Word;
+        }
+
+        private void Answer_TextChanged(object sender, EventArgs e)
+        {
+            var succes = false;
+            if (pictureBox1.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox1.Visible = false; };
+            if (pictureBox2.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox2.Visible = false; };
+            if (pictureBox3.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox3.Visible = false; };
+            if (pictureBox4.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox4.Visible = false; };
+            if (pictureBox5.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox5.Visible = false; };
+            if (pictureBox6.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox6.Visible = false; };
+            if (pictureBox7.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox7.Visible = false; };
+            if (pictureBox8.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox8.Visible = false; };
+            if (pictureBox9.Tag.ToString().ToLower() == Answer.Text.ToLower()) { succes = true; pictureBox9.Visible = false; };
+
+            if (succes)
+            {
+                visibility -= 255 / 9;
+                //Image image = textBox1.Parent.BackgroundImage;
+                //using (Graphics g = Graphics.FromImage(image))
+                //{
+                //    Pen pen = new Pen(Color.FromArgb(visibility, 255, 255, 255), image.Width);
+                //    g.DrawLine(pen, -1, -1, image.Width, image.Height);
+                //    g.Save();
+                //}
+                //textBox1.Parent.BackgroundImage = image;
+                new System.Media.SoundPlayer(Properties.Resources.tada).Play();
+                Answer.Text = "";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox1);
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox2);
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox3);
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox4);
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox5);
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox6);
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox7);
+        }
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox8);
+        }
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            ShowHint(pictureBox9);
+        }
+        private void ShowHint(PictureBox tile)
+        {
+            var ary = tile.Tag.ToString().ToCharArray();
+            Array.Reverse(ary);
+            tile.Parent.Text = new string(ary);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Answer.Text = "";
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
+            pictureBox7.Visible = true;
+            pictureBox8.Visible = true;
+            pictureBox9.Visible = true;
+            visibility = 255;
+            //Image image = button1.Parent.BackgroundImage;
+            //using (Graphics g = Graphics.FromImage(image))
+            //{
+            //    Pen pen = new Pen(Color.FromArgb(visibility, 255, 255, 255), image.Width);
+            //    g.DrawLine(pen, -1, -1, image.Width, image.Height);
+            //    g.Save();
+            //}
+            //button1.Parent.BackgroundImage = image;
+
+        }
+
+        private async void UseSpeech_Click(object sender, EventArgs e)
+        {
+            UseSpeech.Enabled = false;
+            new System.Media.SoundPlayer(Properties.Resources.Speech_On).Play();
+            var resultText = await RecognizeSpeechAsync();
+            //var resultText = (await new SpeechRecognizer(SpeechConfig.FromSubscription(Properties.Settings.Default.SubscriptionKey, Properties.Settings.Default.Region)).RecognizeOnceAsync()).Text;
+            Answer.Text = resultText.TrimEnd('.');// resultText.TrimEnd(".".ToCharArray());
+            new System.Media.SoundPlayer(Properties.Resources.Speech_Off).Play();
+            UseSpeech.Enabled = true;
+        }
+        public static async Task<string> RecognizeSpeechAsync()
+        {
+            Application.UseWaitCursor = true;
+            var config = SpeechConfig.FromSubscription(Properties.Settings.Default.SubscriptionKey, Properties.Settings.Default.Region);
+            config.SpeechRecognitionLanguage = Properties.Settings.Default.SpeechRecognitionLanguage;
+            var recognizer = new SpeechRecognizer(config);
+            var result = await recognizer.RecognizeOnceAsync();
+            Application.UseWaitCursor = false;
+
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                return result.Text;
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                return "NOMATCH: Speech could not be recognized.";
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                var cancellationdetails = "CANCELLED: Reason=" + cancellation.Reason;
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    cancellationdetails += ", ErrorCode=" + cancellation.ErrorCode;
+                    cancellationdetails += ", ErrorDetails=" + cancellation.ErrorDetails;
+                }
+                return cancellationdetails;
+            }
+            return "RECOGNIZESPEECHASYNC: This shouldn't happen";
+        }
+
+    }
+}
